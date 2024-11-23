900dd23242827bd54d22055265905c0de136b59e
¨joythishreddye¨ <evuri.j@northeastern.edu>
2024-11-22 23:29:55 -0500

    add multiply and divide operations in path parameters

--- Lab5/PathParameters.js
+++ Lab5/PathParameters.js
@@ -6,8 +6,21 @@ export default function PathParameters(app) {
     });
     app.get("/lab5/subtract/:a/:b", (req, res) => {
       const { a, b } = req.params;
-      const sum = parseInt(a) - parseInt(b);
-      res.send(sum.toString());
+      const diff = parseInt(a) - parseInt(b);
+      res.send(diff.toString());
+    });
+    app.get("/lab5/multiply/:a/:b", (req, res) => {
+      const { a, b } = req.params;
+      const product = parseInt(a) * parseInt(b);
+      res.send(product.toString());
+    });
+    app.get("/lab5/divide/:a/:b", (req, res) => {
+      const { a, b } = req.params;
+        if (parseInt(b) === 0) {
+            return res.send("Cannot divide by zero");
+        }
+      const division = parseInt(a) / parseInt(b);
+      res.send(division.toString());
     });
   };
   
