b872aedf5f57c9a1a7d0ca7066abc789ac31e7fb
¨joythishreddye¨ <evuri.j@northeastern.edu>
2024-12-07 04:31:45 -0500

    add find users for course route

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,6 +1,7 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
+import * as enrollmentsDao from "../Enrollments/dao.js";
 
 export default function CourseRoutes(app) {
   app.get("/api/courses/:courseId/modules", async(req, res) => {
@@ -50,5 +51,11 @@ export default function CourseRoutes(app) {
     const course = await dao.createCourse(req.body);
     res.json(course);
   });
+  const findUsersForCourse = async (req, res) => {
+    const { courseId } = req.params;
+    const users = await enrollmentsDao.findUsersForEnrolledCourse(courseId);
+    res.json(users);
+  };
+  app.get("/api/courses/:courseId/users", findUsersForCourse);
  
 }
--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,8 +1,6 @@
-import Database from "../Database/index.js";
-import model from "./model.js"; // Ensure that the model.js file exports the required model object
+import model from "./model.js";
 
 export function enrollUserInCourse(userId, courseId) {
-  //console.log(userId, courseId);
   const newEnrollment = { user: userId, course: courseId };
   return model.create(newEnrollment);
 }
@@ -17,6 +15,7 @@ export function findAllEnrollments() {
 return model.find();
 }
 
-export function findUsersForEnrolledCourse(courseId) {
-  return model.find({ course: courseId });
+export async function findUsersForEnrolledCourse(courseId) {
+  const enrollments = await model.find({ course: courseId }).populate('user');
+  return enrollments.map(enrollment => enrollment.user);
 }
